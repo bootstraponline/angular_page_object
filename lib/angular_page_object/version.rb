937e07cdd19f161a3943f52d558549892f244a6b
bootstraponline <code@bootstraponline.com>
2015-06-19 13:53:07 -0400

    Release 1.0.1

--- lib/angular_page_object/version.rb
+++ lib/angular_page_object/version.rb
@@ -1,4 +1,4 @@
 module AngularPageObject
-  VERSION = '1.0.0' unless defined? ::AngularPageObject::VERSION
-  DATE    = '2015-06-06' unless defined? ::AngularPageObject::DATE
+  VERSION = '1.0.1' unless defined? ::AngularPageObject::VERSION
+  DATE    = '2015-06-19' unless defined? ::AngularPageObject::DATE
 end
